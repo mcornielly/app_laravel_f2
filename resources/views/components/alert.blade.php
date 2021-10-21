37898a95e0c803adf70f6e54ed8670e6283648a4
Miguel Angel Cornielly H <mcornielly@gmail.com>
2021-10-21 10:08:03 -0400

    ajustment alert messages in a container

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,9 @@
 @if(session('message'))
-    <div class="col-md-8 mt-3">
-        <div class="alert alert-{{ session('message')[0] }}">
-            {!! session('message')[1] !!}
+    <div class="container">
+        <div class="col-md-8 mt-3">
+            <div class="alert alert-{{ session('message')[0] }}">
+                {!! session('message')[1] !!}
+            </div>
         </div>
     </div>
 @endif
