2d7949e0f87678db5115f16ec92c25f427ace9c0
Miguel Angel Cornielly H <mcornielly@gmail.com>
2021-11-02 22:56:55 -0400

    modified configuration in webpack,mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,12 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.disableNotifications();
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+.sass('resources/sass/app.scss', 'public/css');
 
 
+mix.disableNotifications();
 mix.browserSync({
     proxy: 'http://app_laravel_f2.test',
     port: 3015,
